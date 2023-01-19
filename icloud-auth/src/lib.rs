4241647064c90e639a48ef8597d73515d86a983a
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-01-19 22:29:45 +0000

    move tests

--- icloud-auth/src/lib.rs
+++ icloud-auth/src/lib.rs
@@ -1,4 +1,4 @@
-mod anisette;
+pub mod anisette;
 mod client;
 pub use client::AppleAccount;
 #[derive(Debug)]
@@ -6,29 +6,4 @@ pub enum Error {
     HttpRequest,
     Parse,
     AuthSrp,
-}
-
-// login test
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn new_gsa_auth() {
-        println!("gsa auth test");
-        let password = std::env::var("apple_password").unwrap();
-        let email = std::env::var("apple_email").unwrap();
-        let ad = anisette::AnisetteData::from_url(anisette::SIDELOADLY_ANISETTE).unwrap();
-        print!("{:?}", ad);
-        let appleid_closure = move || (email.clone(), password.clone());
-        // ask console for 2fa code
-        let tfa_closure = || {
-            println!("Enter 2FA code: ");
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-            input
-        };
-        let _ = client::AppleAccount::login(appleid_closure, tfa_closure, ad);
-    }
 }
--- icloud-auth/tests/gsa_auth.rs
+++ icloud-auth/tests/gsa_auth.rs
@@ -1,27 +1,22 @@
-// use icloud_auth::*;
+#[cfg(test)]
+mod tests {
+    use icloud_auth::*;
 
-// #[cfg(test)]
-// mod tests {
-
-//     // use super::*;
-
-//     // #[test]
-//     // fn gsa_auth() {
-//     //     println!("gsa auth test");
-//     //     let password = std::env::var("apple_password").unwrap();
-//     //     let email = std::env::var("apple_email").unwrap();
-//     //     let ad = anisette::AnisetteData::from_url(anisette::SIDELOADLY_ANISETTE).unwrap();
-//     //     print!("{:?}", ad);
-//     //     let _ = request::GsaClient::new(email, password, ad);
-//     // }
-
-//     #[test]
-//     fn gsa_auth() {
-//         println!("gsa auth test");
-//         let password = std::env::var("apple_password").unwrap();
-//         let email = std::env::var("apple_email").unwrap();
-//         let ad = ::anisette::AnisetteData::from_url(anisette::SIDELOADLY_ANISETTE).unwrap();
-//         print!("{:?}", ad);
-//         let _ = icloud_auth::AppleAccount::login(email, password, ad);
-//     }
-// }
+    #[test]
+    fn gsa_auth() {
+        println!("gsa auth test");
+        let password = std::env::var("apple_password").unwrap();
+        let email = std::env::var("apple_email").unwrap();
+        let ad = anisette::AnisetteData::from_url(anisette::SIDELOADLY_ANISETTE).unwrap();
+        print!("{:?}", ad);
+        let appleid_closure = move || (email.clone(), password.clone());
+        // ask console for 2fa code
+        let tfa_closure = || {
+            println!("Enter 2FA code: ");
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            input
+        };
+        let _ = AppleAccount::login(appleid_closure, tfa_closure, ad);
+    }
+}
